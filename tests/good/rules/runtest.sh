6207e80fb90390f71fb1c468cc9ed4b8b40b1a8b
Jamie Strandboge <jamie@ubuntu.com>
2008-01-14 03:18:19 -0500

    ufw: only accept PORT with simple rules test/good/rules: add manpage examples

--- tests/good/rules/runtest.sh
+++ tests/good/rules/runtest.sh
@@ -11,11 +11,23 @@ do_cmd() {
 	echo "" >> $TESTTMP/result
 }
 
+echo "Man page" >> $TESTTMP/result
+do_cmd --dry-run allow 53
+do_cmd --dry-run allow 25:tcp
+do_cmd --dry-run deny to 80:tcp
+do_cmd --dry-run deny from 10.0.0.0/8 to 192.168.0.1 25:tcp
+do_cmd --dry-run deny 53
+do_cmd --dry-run allow 80:tcp
+do_cmd --dry-run allow from 10.0.0.0/8
+do_cmd --dry-run allow from 172.16.0.0/12
+do_cmd --dry-run allow from 192.168.0.0/16
+do_cmd --dry-run deny from 1.2.3.4 to 514:udp
+do_cmd --dry-run allow from 1.2.3.5 5469:udp to 1.2.3.4 5469:udp
+
 echo "SIMPLE" >> $TESTTMP/result
 do_cmd --dry-run allow 25
 do_cmd --dry-run allow 25:tcp
 do_cmd --dry-run allow 25:udp
-do_cmd --dry-run allow 10.0.0.1
 
 echo "TO/FROM" >> $TESTTMP/result
 from="192.168.0.1"
