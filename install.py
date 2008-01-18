c65e4de63e23b9a595ffd686990f4cd3f24088fa
Jamie Strandboge <jamie@ubuntu.com>
2008-01-18 09:51:10 -0500

    adjust files to comply with CopyrightAndLicensing

--- install.py
+++ install.py
@@ -3,21 +3,19 @@
 
 ###########################################################################
 #
-# Copyright (C) 2007 Jamie Strandboge <jamie@ubuntu.com>
+# Copyright (C) 2008 Canonical Ltd.
 #
-#    ufw is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published
-#    by the Free Software Foundation; either version 2 of the License,
-#    or (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3,
+#    as published by the Free Software Foundation.
 #
-#    ufw is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with ufw; if not, write to the Free Software Foundation,
-#    Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###########################################################################
 
@@ -269,7 +267,7 @@ def evalArguments():
 ###############################################################################
 
 
-print appname + " (C) 2007 Jamie Strandboge\n"
+print appname + " (C) 2008 Canonical Ltd.\n"
 
 doImportCheck()
 evalArguments()
--- tests/ipv6/args/runtest.sh
+++ tests/ipv6/args/runtest.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#    Copyright (C) 2008 Canonical Ltd.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3,
+#    as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 sed -i 's/IPV6=no/IPV6=yes/' $TESTPATH/etc/default/ufw
 sed -i 's/disableChecks = False/disableChecks = True/' $TESTPATH/usr/sbin/ufw
 
