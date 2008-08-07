f6d59351e7ce3878f9b69d5d2526f265adb0f61d
Jamie Strandboge <jamie@canonical.com>
2008-08-07 12:04:37 -0400

    bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from popen2 import Popen3
 import sys
 import shutil
 
-ufw_version = '0.19'
+ufw_version = '0.20'
 
 class Install(_install, object):
     '''Override distutils to install the files where we want them.'''
