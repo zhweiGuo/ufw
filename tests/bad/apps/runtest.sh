9191f9266357885105885b90b7e30f0fda75f463
Jamie Strandboge <jamie@lupin>
2008-08-14 06:53:47 -0400

    update tests/bad/apps test and results

--- tests/bad/apps/runtest.sh
+++ tests/bad/apps/runtest.sh
@@ -43,16 +43,16 @@ echo "TESTING APPLICATION INTEGRATION (bad extended rules)" >> $TESTTMP/result
 for target in allow deny limit ; do
     for i in to from ; do
         k="to"
-        if [ "$j" = "to" ]; then
+        if [ "$i" = "to" ]; then
             k="from"
         fi
         for loc in 192.168.0.0/16 any ; do
-            do_cmd "1" --dry-run $target $i $loc app NONEXISTENT
-            do_cmd "1" --dry-run $target $i $loc app Apache proto tcp
-            do_cmd "1" --dry-run $target $i $loc app Apache proto udp
-            do_cmd "1" --dry-run $target $i $loc app 'No Protocol Multi'
-            do_cmd "1" --dry-run $target $i $loc app Samba $k $loc port http
-            do_cmd "1" --dry-run $target $i $loc app Samba $k $loc port 22 proto tcp
+            do_cmd "1" null --dry-run $target $i $loc app NONEXISTENT
+            do_cmd "1" null --dry-run $target $i $loc app Apache proto tcp
+            do_cmd "1" null --dry-run $target $i $loc app Apache proto udp
+            do_cmd "1" null --dry-run $target $i $loc app 'No Protocol Multi'
+            do_cmd "1" null --dry-run $target $i $loc app Samba $k $loc port http
+            do_cmd "1" null --dry-run $target $i $loc app Samba $k $loc port 22 proto tcp
         done
     done
 done
