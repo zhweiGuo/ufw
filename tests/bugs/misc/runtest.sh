2b89697730f4b40beb5db5be19e66df08a7fa8cf
Jamie Strandboge <jamie@canonical.com>
2009-10-19 18:38:33 -0500

    split out application rules tests

--- tests/bugs/misc/runtest.sh
+++ tests/bugs/misc/runtest.sh
@@ -32,61 +32,6 @@ sed -i 's/import ufw.frontend/import ufw.nonexistent/' $TESTPATH/usr/sbin/ufw
 do_cmd "1" help
 sed -i 's/import ufw.nonexistent/import ufw.frontend/' $TESTPATH/usr/sbin/ufw
 
-echo "Bug (Samba IPV4 tuple text wrong when IPV6 is enabled" >> $TESTTMP/result
-sed -i 's/IPV6=.*/IPV6=yes/' $TESTPATH/etc/default/ufw
-do_cmd "0" allow in on eth1 to any app Samba
-grep -A2 "tuple" $TESTSTATE/user.rules >> $TESTTMP/result
-grep -A2 "tuple" $TESTSTATE/user6.rules >> $TESTTMP/result
-do_cmd "0" null delete allow in on eth1 to any app Samba
-sed -i 's/IPV6=.*/IPV6=no/' $TESTPATH/etc/default/ufw
-
-echo "Bug (inserted Samba rules out of order when IPV6 is enabled" >> $TESTTMP/result
-sed -i 's/IPV6=.*/IPV6=yes/' $TESTPATH/etc/default/ufw
-do_cmd "0" allow in on eth0
-do_cmd "0" allow to 192.168.0.2
-do_cmd "0" allow to 192.168.0.3
-do_cmd "0" allow in on eth1
-do_cmd "0" allow in on eth2
-do_cmd "0" insert 8 deny to any app Bind9
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-grep "^-A .*user-input" $TESTSTATE/user6.rules >> $TESTTMP/result
-
-do_cmd "0" delete deny to any app Bind9
-do_cmd "0" insert 8 deny to any app Samba
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-grep "^-A .*user-input" $TESTSTATE/user6.rules >> $TESTTMP/result
-
-# this insert should look the same as the above
-do_cmd "0" delete deny to any app Samba
-do_cmd "0" insert 5 deny to any app Bind9
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-grep "^-A .*user-input" $TESTSTATE/user6.rules >> $TESTTMP/result
-
-do_cmd "0" delete deny to any app Bind9
-do_cmd "0" insert 5 deny to any app Samba
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-grep "^-A .*user-input" $TESTSTATE/user6.rules >> $TESTTMP/result
-
-do_cmd "0" delete allow in on eth0
-do_cmd "0" delete allow to 192.168.0.2
-do_cmd "0" delete allow to 192.168.0.3
-do_cmd "0" delete allow in on eth1
-do_cmd "0" delete allow in on eth2
-do_cmd "0" delete deny to any app Samba
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-grep "^-A .*user-input" $TESTSTATE/user6.rules >> $TESTTMP/result
-sed -i 's/IPV6=.*/IPV6=no/' $TESTPATH/etc/default/ufw
-
-echo "Bug #407810" >> $TESTTMP/result
-cp "$TESTPATH/etc/ufw/applications.d/samba" "$TESTPATH/etc/ufw/applications.d/bug407810"
-sed -i 's/Samba/bug407810/' "$TESTPATH/etc/ufw/applications.d/bug407810"
-do_cmd "0" app info bug407810
-do_cmd "0" null allow bug407810
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-rm -f "$TESTPATH/etc/ufw/applications.d/bug407810"
-do_cmd "0" null delete allow bug407810
-grep "^-A .*user-input" $TESTSTATE/user.rules >> $TESTTMP/result
-
 echo "Bug #430053" >> $TESTTMP/result
 # files permissions are overridden when root
 expected="1"
