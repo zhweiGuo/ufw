799167f7cd5d6291e1a7df79b0699fa3caf4fa41
Jamie Strandboge <jamie@canonical.com>
2010-08-21 15:52:46 -0500

    enable tests/good/reports/runtest.sh even though it fails.
    TODO:
    work toward the output in tests/good/reports/result
    verify ports 33257, 5353 and 68 in tests/good/reports/result

--- tests/good/reports/runtest.sh
+++ tests/good/reports/runtest.sh
@@ -16,21 +16,116 @@
 
 source "$TESTPATH/../testlib.sh"
 
-exit 0
+# The show listening test is a regression test that has:
+# eth0 = 10.0.2.9   fe80::0211:aaaa:bbbb
+# eth1 = 10.0.2.101 fe80::0212:cccc:dddd
+#
+# With the following open ports (port 68 is listed twice due to eth0 and eth1):
+#tcp        0      0 0.0.0.0:22
+#tcp        0      0 127.0.0.1:631
+#tcp6       0      0 :::22
+#tcp6       0      0 ::1:631
+#udp        0      0 0.0.0.0:68
+#udp        0      0 0.0.0.0:68
+#udp        0      0 0.0.0.0:33257
+#udp        0      0 0.0.0.0:5353
+#udp        0      0 10.0.2.101:123
+#udp        0      0 10.0.2.9:123
+#udp        0      0 127.0.0.1:123
+#udp        0      0 0.0.0.0:123
+#udp6       0      0 fe80::212:cccc:dddd:123
+#udp6       0      0 fe80::211:aaaa:bbbb:123
+#udp6       0      0 ::1:123
+#udp6       0      0 :::123
+#
+# So this test will modify util.py to:
+# - look at ./netstat.enlp instead of calling 'netstat -enlp' directly
+# - to use a fake /proc/net/if_inet6 and /proc/net/dev
+# - to hard code the addressed for eth0 and eth1
+#
+# The rules that are created should create the following 'status numbered'
+# output:
+# [ 1] 123                        ALLOW IN    Anywhere
+# [ 2] OpenNTPD                   ALLOW IN    Anywhere
+# [ 3] 123/tcp                    ALLOW IN    Anywhere
+# [ 4] Anywhere                   ALLOW IN    Anywhere
+# [ 5] Anywhere/udp               ALLOW IN    Anywhere/udp
+# [ 6] Anywhere/tcp               ALLOW IN    Anywhere/tcp
+# [ 7] 10.0.2.101                 ALLOW IN    Anywhere
+# [ 8] 10.0.2.9                   ALLOW IN    Anywhere
+# [ 9] 10.0.0.0/16                ALLOW IN    Anywhere
+# [10] 10.0.2.0/24                ALLOW IN    Anywhere
+# [11] 10.0.3.0/24                ALLOW IN    Anywhere
+# [12] 10.0.2.101 123             ALLOW IN    Anywhere
+# [13] 10.0.0.0/16 123            ALLOW IN    Anywhere
+# [14] 10.0.2.0/24 123            ALLOW IN    Anywhere
+# [15] 10.0.3.0/24 123            ALLOW IN    Anywhere
+# [16] 10.0.2.101 123/udp         ALLOW IN    Anywhere
+# [17] 10.0.0.0/16 OpenNTPD       ALLOW IN    Anywhere
+# [18] 10.0.2.0/24 123/udp        ALLOW IN    Anywhere
+# [19] 10.0.3.0/24 123/udp        ALLOW IN    Anywhere
+# [20] 10.0.2.101 123/tcp         ALLOW IN    Anywhere
+# [21] 10.0.0.0/16 123/tcp        ALLOW IN    Anywhere
+# [22] 10.0.2.0/24 123/tcp        ALLOW IN    Anywhere
+# [23] 10.0.3.0/24 123/tcp        ALLOW IN    Anywhere
+# [24] 123                        ALLOW OUT   Anywhere (out)
+# [25] 123/udp                    ALLOW OUT   Anywhere (out)
+# [26] 123/tcp                    ALLOW OUT   Anywhere (out)
+# [27] Anywhere on eth0           ALLOW IN    Anywhere
+# [28] Anywhere/udp on eth0       ALLOW IN    Anywhere/udp
+# [29] Anywhere/tcp on eth0       ALLOW IN    Anywhere/tcp
+# [30] 10.0.2.101 on eth0         ALLOW IN    Anywhere
+# [31] 10.0.2.9 on eth0           ALLOW IN    Anywhere
+# [32] 10.0.0.0/16 on eth0        ALLOW IN    Anywhere
+# [33] 10.0.2.0/24 on eth0        ALLOW IN    Anywhere
+# [34] 10.0.3.0/24 on eth0        ALLOW IN    Anywhere
+# [35] 10.0.2.101 123 on eth0     ALLOW IN    Anywhere
+# [36] 10.0.0.0/16 123 on eth0    ALLOW IN    Anywhere
+# [37] 10.0.2.0/24 123 on eth0    ALLOW IN    Anywhere
+# [38] 10.0.3.0/24 123 on eth0    ALLOW IN    Anywhere
+# [39] 10.0.2.101 123/udp on eth0 ALLOW IN    Anywhere
+# [40] 10.0.0.0/16 OpenNTPD on eth0 ALLOW IN    Anywhere
+# [41] 10.0.2.0/24 123/udp on eth0 ALLOW IN    Anywhere
+# [42] 10.0.3.0/24 123/udp on eth0 ALLOW IN    Anywhere
+# [43] 10.0.2.101 123/tcp on eth0 ALLOW IN    Anywhere
+# [44] 10.0.0.0/16 123/tcp on eth0 ALLOW IN    Anywhere
+# [45] 10.0.2.0/24 123/tcp on eth0 ALLOW IN    Anywhere
+# [46] 10.0.3.0/24 123/tcp on eth0 ALLOW IN    Anywhere
+# [47] 123                        ALLOW IN    Anywhere (v6)
+# [48] OpenNTPD (v6)              ALLOW IN    Anywhere (v6)
+# [49] 123/tcp                    ALLOW IN    Anywhere (v6)
+# [50] Anywhere (v6)              ALLOW IN    Anywhere (v6)
+# [51] Anywhere/udp (v6)          ALLOW IN    Anywhere/udp (v6)
+# [52] Anywhere/tcp (v6)          ALLOW IN    Anywhere/tcp (v6)
+# [53] fe80::211:aaaa:bbbb        ALLOW IN    Anywhere (v6)
+# [54] fe80::211:aaaa:bbbb 123    ALLOW IN    Anywhere (v6)
+# [55] fe80::211:aaaa:bbbb 123/udp ALLOW IN    Anywhere (v6)
+# [56] fe80::211:aaaa:bbbb 123/tcp ALLOW IN    Anywhere (v6)
+# [57] 123                        ALLOW OUT   Anywhere (v6) (out)
+# [58] 123/udp                    ALLOW OUT   Anywhere (v6) (out)
+# [59] 123/tcp                    ALLOW OUT   Anywhere (v6) (out)
+# [60] Anywhere (v6) on eth0      ALLOW IN    Anywhere (v6)
+# [61] Anywhere/udp (v6) on eth0  ALLOW IN    Anywhere/udp (v6)
+# [62] Anywhere/tcp (v6) on eth0  ALLOW IN    Anywhere/tcp (v6)
+# [63] fe80::211:aaaa:bbbb on eth0 ALLOW IN    Anywhere (v6)
+# [64] fe80::211:aaaa:bbbb 123 on eth0 ALLOW IN    Anywhere (v6)
+# [65] fe80::211:aaaa:bbbb 123/udp on eth0 ALLOW IN    Anywhere (v6)
+# [66] fe80::211:aaaa:bbbb 123/tcp on eth0 ALLOW IN    Anywhere (v6)
 
 echo "show listening" >> $TESTTMP/result
 echo "(update util.py to use our cached output)" >> $TESTTMP/result
 sed -i "s#rc, report = cmd.* '-enlp'.*#rc, report = cmd(['cat', '$TESTPATH/../good/reports/netstat.enlp'])#" $TESTPATH/lib/python/ufw/util.py
-sed -i "s#item\['exe'\] = get_exe.*#item['exe'] = 'REMOVED_FOR_UFW_TEST'#" $TESTPATH/lib/python/ufw/util.py
+sed -i "s#item\['exe'\] = get_exe.*#item['exe'] = 'EXE_FILTERED_OUT_OF_TEST'#" $TESTPATH/lib/python/ufw/util.py
 sed -i "s#proc = '/proc/net/if_inet6'#proc = '$TESTPATH/../good/reports/proc_net_if_inet6'#" $TESTPATH/lib/python/ufw/util.py
 sed -i "s#proc = '/proc/net/dev'#proc = '$TESTPATH/../good/reports/proc_net_dev'#" $TESTPATH/lib/python/ufw/util.py
 sed -i "s#\(.*\)\(addr = .* 0x8915,.*\)#\\1if ifname == 'eth0':\n\\1\\1addr = '10.0.2.9'\n\\1elif ifname == 'eth1':\n\\1\\1addr = '10.0.2.101'\n\\1else:\n\\1\\1raise IOError\n    return normalize_address(addr, v6)[0]\n    \\2#" $TESTPATH/lib/python/ufw/util.py
 
-do_cmd "0" show listening
-
-echo "show listening with rules" >> $TESTTMP/result
 sed -i "s/IPV6=.*/IPV6=yes/" $TESTPATH/etc/default/ufw
 
+echo "show listening with no rules" >> $TESTTMP/result
+do_cmd "0" show listening
+
+echo "Add rules for test" >> $TESTTMP/result
 for i in "" "in on eth0" ; do
     if [ -z "$i" ]; then
         do_cmd "0" null allow in 123
@@ -72,6 +167,7 @@ for i in "" "in on eth0" ; do
     do_cmd "0" null allow $i to fe80::211:aaaa:bbbb:d54c port 123 proto tcp
 done
 
+echo "show listening with rules" >> $TESTTMP/result
 do_cmd "0" show listening
 
 exit 0
