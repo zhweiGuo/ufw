159680df8500b87c27f6a4040a2f88332ca8811d
jdstrand <JamieStrandboge>
2014-02-19 11:50:45 -0600

    run_tests.sh: set ip forwarding sysctl values to predictable state and reset them on script end

--- run_tests.sh
+++ run_tests.sh
@@ -159,6 +159,17 @@ rm -rf "$CUR/src/__pycache__"
 
 # Functional tests
 echo "= Functional Tests ="
+
+# Explicitly disable IP forwarding here, since some tests assume it is
+# disabled. IP forwarding will be re-enabled in the individual tests
+# that require it.
+orig_ip_forward=`sysctl net.ipv4.ip_forward 2>/dev/null | cut -d ' ' -f 2`
+orig_ipv6_forwarding_default=`sysctl net.ipv6.conf.default.forwarding 2>/dev/null | cut -d ' ' -f 2`
+orig_ipv6_forwarding_all=`sysctl net.ipv6.conf.all.forwarding 2>/dev/null | cut -d ' ' -f 2`
+sysctl -w net.ipv4.ip_forward=0 2>/dev/null || true
+sysctl -w net.ipv6.conf.default.forwarding=0 2>/dev/null || true
+sysctl -w net.ipv6.conf.all.forwarding=0 2>/dev/null || true
+
 for class in $tests
 do
     if [ "$class" = "unit" ]; then
@@ -267,6 +278,14 @@ do
         fi
     done
 done
+
+# Restore IP forwarding
+test -n "$orig_ip_forward" && \
+    sysctl -w net.ipv4.ip_forward="$orig_ip_forward" 2>/dev/null || true
+test -n "$orig_ipv6_forwarding_default" && \
+    sysctl -w net.ipv6.conf.default.forwarding="$orig_ipv6_forwarding_default" 2>/dev/null || true
+test -n "$orig_ipv6_forwarding_all" && \
+    sysctl -w net.ipv6.conf.all.forwarding="$orig_ipv6_forwarding_all" 2>/dev/null || true
 
 if [ -d "$TESTPATH" ]; then
     rm -rf "$TESTPATH"
