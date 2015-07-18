463d16f1c02ebda50cdeca8e88fbee8fa34faba2
Jamie Strandboge <jamie@ubuntu.com>
2015-07-18 10:01:11 -0500

    add route rules to --help

--- src/frontend.py
+++ src/frontend.py
@@ -101,6 +101,9 @@ Usage: %(progname)s %(command)s
  %(limit)-31s add limit %(rule)s
  %(delete)-31s delete %(urule)s
  %(insert)-31s insert %(urule)s at %(number)s
+ %(route)-31s add route %(urule)s
+ %(route-delete)-31s delete route %(urule)s
+ %(route-insert)-31s insert route %(urule)s at %(number)s
  %(reload)-31s reload firewall
  %(reset)-31s reset firewall
  %(status)-31s show firewall status
@@ -130,6 +133,9 @@ Usage: %(progname)s %(command)s
          'delete': "delete RULE|NUM", \
          'urule': "RULE", \
          'insert': "insert NUM RULE", \
+         'route': "route RULE", \
+         'route-delete': "route delete RULE|NUM", \
+         'route-insert': "route insert NUM RULE", \
          'number': "NUM", \
          'reload': "reload", \
          'reset': "reset", \
