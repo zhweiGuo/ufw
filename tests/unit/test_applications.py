3930d39691089dfcbfa99d83f541df8336cf6335
Jamie Strandboge <jamie@ubuntu.com>
2015-07-24 16:01:43 -0500

    tests/unit/test_applications.py: fix whitespace alignment

--- tests/unit/test_applications.py
+++ tests/unit/test_applications.py
@@ -107,16 +107,16 @@ class ApplicationsTestCase(unittest.TestCase):
                     {'title': 'bad protocol - esp',
                      'description': 'desc',
                      'ports': '80/esp'},
-                     {'title': 'bad protocol - gre',
-                      'description': 'desc',
-                      'ports': '80/gre'},
-                     {'title': 'bad protocol - igmp',
-                      'description': 'desc',
-                      'ports': '80/igmp'},
-                     {'title': 'bad protocol - ipv6',
-                      'description': 'desc',
-                      'ports': '80/ipv6'},
-                     ]
+                    {'title': 'bad protocol - gre',
+                     'description': 'desc',
+                     'ports': '80/gre'},
+                    {'title': 'bad protocol - igmp',
+                     'description': 'desc',
+                     'ports': '80/igmp'},
+                    {'title': 'bad protocol - ipv6',
+                     'description': 'desc',
+                     'ports': '80/ipv6'},
+                    ]
         for p in profiles:
             print(" %s" % p)
             tests.unit.support.check_for_exception(self,
