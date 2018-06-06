703fa28f1e41bb9bee4563b861f2f8448294a8df
Jamie Strandboge <jamie@ubuntu.com>
2018-06-06 16:23:57 -0500

    Revert "add some input verification of nat rules"
    This reverts commit b1f0ad2ac7c3c0087c9ac722b87874a9f80bad61.

--- src/common.py
+++ src/common.py
@@ -371,31 +371,25 @@ class UFWRule:
             err_msg = _("Unsupported direction '%s'") % (direction)
             raise UFWError(err_msg)
 
-    def set_dnat(self, addr):
+    def set_dnat(self, dnat):
         '''Sets dnat of the rule'''
-        tmp = addr.lower()
-        if tmp != "rdr" and ('/' in tmp or \
-                not ufw.util.valid_address(tmp, "any")):
-            err_msg = _("Bad nat-dst address %s" % addr)
-            raise UFWError(err_msg)
-        self.dnat = tmp
+        # TODO: verify
+        self.dnat = dnat
 
-    def set_dnat_port(self, port):
+    def set_dnat_port(self, dnat_port):
         '''Sets dnat_port of the rule'''
-        self.dnat_port = self._verify_nat_port(port)
+        # TODO: verify
+        self.dnat_port = dnat_port
 
-    def set_snat(self, addr):
+    def set_snat(self, snat):
         '''Sets snat of the rule'''
-        tmp = addr.lower()
-        if tmp != "masq" and ('/' in tmp or \
-                not ufw.util.valid_address(tmp, "any")):
-            err_msg = _("Bad nat-src address '%s'" % addr)
-            raise UFWError(err_msg)
-        self.snat = tmp
+        # TODO: verify
+        self.snat = snat
 
-    def set_snat_port(self, port):
+    def set_snat_port(self, snat_port):
         '''Sets snat_port of the rule'''
-        self.snat_port = self._verify_nat_port(port)
+        # TODO: verify
+        self.snat_port = snat_port
 
     def get_comment(self):
         '''Get decoded comment of the rule'''
@@ -622,24 +616,6 @@ class UFWRule:
         if addr == "::/0" or addr == "0.0.0.0/0":
             return True
         return False
-
-    def _verify_nat_port(self, port):
-        err_msg = _("Bad port '%s' for nat") % (port)
-        if re.match(r'^\d+-\d+$', port):
-            # port range
-            ran = port.split('-')
-            for i in ran:
-                if int(i) < 1 or int(i) > 65535:
-                    raise UFWError(err_msg)
-            if int(ran[0]) >= int(ran[1]):
-                raise UFWError(err_msg)
-        elif re.match('^\d+$', port):
-            if int(port) < 1 or int(port) > 65535:
-                raise UFWError(err_msg)
-        else:
-            raise UFWError(err_msg)
-
-        return port
 
     def get_app_tuple(self):
         '''Returns a tuple to identify an app rule. Tuple is:
