zhweiGuo/ufw

commit 3fd1b3c092defce2e9e2fe83c11cf6456bf9bc04

clean up some parser error logic and have 100% coverage for parser.py

src/parser.py
@@ -536,12 +536,8 @@ class UFWCommandRouteRule(UFWCommandRule):
             idx = argv.index('delete')
             err_msg = ""
             if len(argv) > idx:
-                try:
-                    int(argv[idx + 1])
-                    err_msg = _("'route delete NUM' unsupported. Use 'delete NUM' instead.")
-                    raise UFWError(err_msg)
-                except ValueError:
-                    pass
+                err_msg = _("'route delete NUM' unsupported. Use 'delete NUM' instead.")
+                raise UFWError(err_msg)
 
         # Let's use as much as UFWCommandRule.parse() as possible. The only
         # difference with our rules is that argv[0] is 'route' and we support
@@ -843,7 +839,7 @@ class UFWParser:
                     # argument to be valid and used
                     if isinstance(self.commands[i][cmd], UFWCommandRule) and \
                        getattr(self.commands[i][cmd], 'type') != 'rule':
-                        continue
+                        continue  # pragma: nocover
                     type = i
                     break
             if type == "":

tests/unit/test_parser.py
@@ -75,6 +75,12 @@ class ParserTestCase(unittest.TestCase):
                                                    c.help,
                                                    [])
 
+    def test_ufwcommand_parse_basic_help(self):
+        '''Test parser.parse_command() - help'''
+        pr = self.parser.parse_command(['help'])
+        search = repr("action='help'\n")
+        self.assertTrue(str(pr) == search, "'%s' != '%s'" % (str(pr), search))
+
     def test_ufwcommand_parse(self):
         '''Test UFWCommand.parse()'''
         c = ufw.parser.UFWCommand('basic', 'status')
@@ -524,6 +530,8 @@ class ParserTestCase(unittest.TestCase):
                 (['route', 'allow', 'to', '192.168.0.0/16', 'app', 'Samba',
                   'from', '192.168.0.0/16', 'port', 'tcpmux'],
                   ufw.common.UFWError),
+                (['rule', 'allow', '22', 'comment'], ufw.common.UFWError),
+                (['route', 'delete', '1'], ufw.common.UFWError),
                ]
         for cmd, exception in cmds:
             #print(" ".join(cmd))

commit a2da2ae36f99fa9472d9bf987abb54716d3e63dd

src/parser.py: fix logic and tests mistakenly broken in 3fd1b3c0

src/parser.py
@@ -536,8 +536,14 @@ class UFWCommandRouteRule(UFWCommandRule):
             idx = argv.index('delete')
             err_msg = ""
             if len(argv) > idx:
-                err_msg = _("'route delete NUM' unsupported. Use 'delete NUM' instead.")
-                raise UFWError(err_msg)
+                try:
+                    # 'route delete NUM' is unsupported
+                    int(argv[idx + 1])
+                    err_msg = _("'route delete NUM' unsupported. Use 'delete NUM' instead.")
+                    raise UFWError(err_msg)
+                except ValueError:
+                    # 'route delete RULE' is supported
+                    pass
 
         # Let's use as much as UFWCommandRule.parse() as possible. The only
         # difference with our rules is that argv[0] is 'route' and we support

tests/unit/test_parser.py
@@ -421,29 +421,31 @@ class ParserTestCase(unittest.TestCase):
     def test_misc_rules_parse(self):
         '''Test rule syntax - miscellaneous'''
         cmds = [
-                ['rule', 'delete', '1'],
-                ['rule', 'delete', 'allow', '22'],
-                ['rule', 'deny', 'from', 'any', 'port', 'domain', 'to', \
-                 'any', 'port', 'tftp'],
-                ['rule', 'allow', 'to', 'any', 'proto', 'gre'],
-                ['rule', 'deny', 'to', 'any', 'proto', 'ipv6'],
-                ['rule', 'allow', 'to', 'any', 'proto', 'igmp'],
-                ['rule', 'reject', 'to', 'any', 'proto', 'esp'],
-                ['rule', 'deny', 'to', '224.0.0.1', 'proto', 'igmp'],
-                ['rule', 'deny', 'in', 'on', 'eth0', 'to', '224.0.0.1', \
-                 'proto', 'igmp'],
-                ['rule', 'allow', 'in', 'on', 'eth0', 'to', '192.168.0.1', \
-                 'proto', 'gre'],
-                ['rule', 'deny', 'to', 'any', 'proto', 'ah'],
-                ['rule', 'allow', 'out', 'on', 'br_lan'],
+                ['delete', 'allow', '22'],
+                ['deny', 'from', 'any', 'port', 'domain', 'to', 'any', \
+                 'port', 'tftp'],
+                ['allow', 'to', 'any', 'proto', 'gre'],
+                ['deny', 'to', 'any', 'proto', 'ipv6'],
+                ['allow', 'to', 'any', 'proto', 'igmp'],
+                ['reject', 'to', 'any', 'proto', 'esp'],
+                ['deny', 'to', '224.0.0.1', 'proto', 'igmp'],
+                ['deny', 'in', 'on', 'eth0', 'to', '224.0.0.1', 'proto', \
+                 'igmp'],
+                ['allow', 'in', 'on', 'eth0', 'to', '192.168.0.1', 'proto', \
+                 'gre'],
+                ['deny', 'to', 'any', 'proto', 'ah'],
+                ['allow', 'out', 'on', 'br_lan'],
                ]
         count = 0
-        for cmd in cmds:
-            #print(" ".join(cmd))
-            count += 1
-            # Note, parser.parse_command() modifies its arg, so pass a copy of
-            # the cmd, not a reference
-            self.parser.parse_command(cmd + [])
+        for rtype in ['route', 'rule']:
+            if rtype == 'rule':
+                cmds.append(['delete', '1'])
+            for cmd in cmds:
+                #print(" ".join(cmd))
+                count += 1
+                # Note, parser.parse_command() modifies its arg, so pass a copy of
+                # the cmd, not a reference
+                self.parser.parse_command([rtype] + cmd)
 
     def test_rule_bad_syntax(self):
         '''Test rule syntax - bad'''